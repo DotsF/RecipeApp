d8521ddd46f1e52341f239412ecde0e00f404ad3
kokihara <nov17hk@gmail.com>
2023-12-27 16:19:37 +0900

    レシピ閲覧Viewの作成①

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -124,7 +124,7 @@ class RecipeController extends Controller
         $recipe = Recipe::find($id);
         $recipe->increment('views'); //PV数を増やす
         //リレーションで材料とステップを取得
-        dd($recipe);
+        // dd($recipe);
         return view('recipes.show', compact('recipe'));
     }
 
